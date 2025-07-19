5995c97edda94bca5497d230354306fc32534d01
Muhammad Rehan <rehanch642@gmail.com>
2025-07-19 21:49:06 +0500

    update product flow

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -17,3 +17,10 @@ class ProductOut(ProductBase):
 
     class Config:
         orm_mode = True
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    sku: Optional[str] = None
+    price: Optional[float] = None
+    stock: Optional[int] = None
+    category_id: Optional[int] = None
